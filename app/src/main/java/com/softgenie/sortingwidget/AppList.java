3fbe8036df90e3f24f4e9a5114c061594863cd89
BlueSky <bluesky01001@naver.com>
2024-05-06 20:52:30 +0900

    사용자 엑세스 허용 구현

--- app/src/main/java/com/softgenie/sortingwidget/AppList.java
+++ app/src/main/java/com/softgenie/sortingwidget/AppList.java
@@ -7,7 +7,6 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.graphics.drawable.Drawable;
-
 import java.util.ArrayList;
 import java.util.List;
 
